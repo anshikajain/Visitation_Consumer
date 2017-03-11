05d42780c80e37ff40d1a57c5fbcde4f1b2e3ca5
anshikajain <anshika.jain@xad.com>
2017-03-10 16:10:52 -0800

    Update configParser.py

--- Config/configParser.py
+++ Config/configParser.py
@@ -4,7 +4,7 @@ from ConfigParser import SafeConfigParser
 parser = SafeConfigParser()
 # locate Excel sheet
 conf_dir = os.path.dirname(os.path.realpath(__file__))
-conf_dir = conf_dir + 'config.cfg'
+conf_dir = conf_dir + '/config.cfg'
 print "My conf_dir is", conf_dir
 parser.read(conf_dir)
 
